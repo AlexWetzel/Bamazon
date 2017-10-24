88fdaca578a77a788b39e026f086cc6fbe47396d
AlexWetzel <wetzela@live.com>
2017-10-24 13:17:14 -0400

    propmts working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var Table = require("cli-table");
+var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
 	host:"localhost",
@@ -10,18 +11,42 @@ var connection = mysql.createConnection({
 });
 
 var table = new Table({
-    head: ['item #', 'product name', 'department', 'price', 'quantity']
+    head: ['item ID', 'product name', 'department', 'price', 'quantity']
   , colWidths: [8, 60, 16, 8, 10]
 });
 
-connection.query("SELECT * FROM products", function(err, res){
-	if (err) throw err;
+function displayStock() {
+	connection.query("SELECT * FROM products", function(err, res){
+		if (err) throw err;
+		for (var i = 0; i < res.length; i++) {
+			table.push(Object.keys(res[i]).map(function(key) {return res[i][key];}));
+		}
+		console.log(table.toString());
+		connection.end();
+		promptCustomer();
+	});
+};
 
-	for (var i = 0; i < res.length; i++) {
-		table.push(Object.keys(res[i]).map(function(key) {return res[i][key];}));
-	}
-	// console.log(Object.keys(res[0]).map(function(key) {return res[0][key];}))
-	
-	console.log(table.toString());
-	connection.end();
-})
+function promptCustomer() {
+	inquirer.prompt([
+	{
+		type: "input",
+		name: "itemID",
+		message: "Select the item ID for the product you want."
+	},
+	{
+		type: "input",
+		name: "quantity",
+		message: "How many would you like to purchase?"
+	}		
+		]).then(function (answers) {
+
+		console.log("This works")
+		console.log(answers.itemID)
+		console.log(answers.quantity)
+    
+	});
+}
+
+displayStock();
+//Prompts the user for the item ID, then asks how many units they want	        
